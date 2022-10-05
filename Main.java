bb75e8dc6a091c66dddcbc3a90a2cd93518331ab
Beerix77 <110314122+Beerix77@users.noreply.github.com>
2022-10-05 21:42:24 +1100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
         //String test = "d4&5e3Z8";
         Scanner keyboard = new Scanner(System.in);
         String test = keyboard.nextLine();
-
+        
+        //Split String into Array
         String[] arr = test.split("");
 
+        // initialise variable x for counting index
         int x = 0;
         for (int j = 1; j <= arr.length; j+=2) {
             int multiplier = Integer.parseInt(arr[j]);
@@ -19,4 +22,3 @@ public class Main {
         }
     }
 }
-
