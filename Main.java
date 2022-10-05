6f2db26f914819d1aade93956ad26499e78ee86b
Beerix77 <110314122+Beerix77@users.noreply.github.com>
2022-10-05 21:26:52 +1100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //String test = "d4&5e3Z8";
+        Scanner keyboard = new Scanner(System.in);
+        String test = keyboard.nextLine();
+
+        String[] arr = test.split("");
+
+        int x = 0;
+        int y = 1;
+
+        for (int j = 0; j <= arr.length - 1; j+=2) {
+            int multiplier = Integer.parseInt(arr[y]);
+            for (int i = 1; i <= multiplier; i++) {
+            System.out.print(arr[x]);
+        }
+        x += 2;
+        y += 2;
+    }
+}}
