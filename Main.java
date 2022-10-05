1a09e9680310c89b55db37a599ebecea546a7e0b
Beerix77 <110314122+Beerix77@users.noreply.github.com>
2022-10-05 21:29:29 +1100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //String test = "d4&5e3Z8";
+        
+        //String test = "d4&5e3Z8";     -> random String for ease of testing
+        
+        // Keyboard input setup
         Scanner keyboard = new Scanner(System.in);
         String test = keyboard.nextLine();
 
+        // Create an Array from inputed string
         String[] arr = test.split("");
 
+        // initialise variables for counting index etc
         int x = 0;
         int y = 1;
 
